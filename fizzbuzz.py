beb177eb8950af29eafe97c2c481ff4ade2cbd09
Adam <adam.j.mccor@gmail.com>
2022-05-30 17:29:55 +0100

    Python FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+def fizzbuzz(number):
+  
+  for i in (range(1,number)):
+    
+    if i % 15 == 0:
+        print('FizzBuzz')
+    elif i % 5 == 0:
+        print('Buzz')
+    elif (i % 3) == 0:
+        print('Fizz')
+    else:
+        print(i)
+   
+fizzbuzz(20)
+        
+        
+
